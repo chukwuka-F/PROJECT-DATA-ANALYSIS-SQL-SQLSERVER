9c10b5983c702b8e36a098e62c94f57f5e362286
CHUKWUKA FRANKLIN <133849372+chukwuka-F@users.noreply.github.com>
2023-05-21 10:14:02 +0100

    Update and rename FRIED RICE ANALYSIS.sql to FOOD SALES ANALYSIS.sql

--- FOOD SALES ANALYSIS.sql
+++ FOOD SALES ANALYSIS.sql
@@ -4,42 +4,52 @@ describe rice_project.fried_rice_sales_2021;
 
 looking at data to be using for 2021
 select ID, month, quantity_produced, quantity_supplied, total_buyers
-select ID, month, quantity_produced, quantity_supplied, total_buyers from rice_project.fried_rice_sales_2021;
+select ID, month, quantity_produced, quantity_supplied, total_buyers 
+from rice_project.fried_rice_sales_2021;
 
 calculate leftover after sales each month for 2021
 select ID, month, quantity_produced, quantity_supplied, total_buyers, (quantity_produced-quantity_supplied) 
-as monthly_left_over from rice_project.fried_rice_sales_2021;
+as monthly_left_over 
+from rice_project.fried_rice_sales_2021;
 
 calculate percentage of quantity supplied each month for 2021
 select ID, month, quantity_produced, quantity_supplied, total_buyers, (quantity_supplied/quantity_produced)*100 
-as percentage_monthly_supplied from rice_project.fried_rice_sales_2021;
+as percentage_monthly_supplied 
+from rice_project.fried_rice_sales_2021;
 
 calculate percentage of quantity leftover each month for 2021
 select ID, month, quantity_produced, quantity_supplied, total_buyers, 100-(quantity_supplied/quantity_produced)*100 
-as percentage_monthly_leftover from rice_project.fried_rice_sales_2021;
+as percentage_monthly_leftover 
+from rice_project.fried_rice_sales_2021;
 
 
 calculate total quantity produced in 2021
 select sum(quantity_produced) 
-as total_yearly_produced from rice_project.fried_rice_sales_2021;
+as total_yearly_produced 
+from rice_project.fried_rice_sales_2021;
 
 
 calculate total quantity supplied in 2021
-select sum(quantity_supplied) as total_yearly_supplied from rice_project.fried_rice_sales_2021;
+select sum(quantity_supplied) 
+as total_yearly_supplied 
+from rice_project.fried_rice_sales_2021;
 
 calculate total quantity of leftover incurred in 2021
 select sum(quantity_produced)-sum(quantity_supplied) 
-as total_yearly_leftover from rice_project.fried_rice_sales_2021;
+as total_yearly_leftover 
+from rice_project.fried_rice_sales_2021;
 
 
 calculate amount sold each month in 2021
 select ID, month, quantity_produced, quantity_supplied, total_buyers, price_per_unit, (quantity_supplied*price_per_unit) 
-as amount_sold from rice_project.fried_rice_sales_2021;
+as amount_sold 
+from rice_project.fried_rice_sales_2021;
 
 
 to calculate monthly revenue for 2021
 select (quantity_supplied*price_per_unit)-(quantity_produced-quantity_supplied)*price_per_unit 
-as monthly_revenue from rice_project.fried_rice_sales_2021;
+as monthly_revenue 
+from rice_project.fried_rice_sales_2021;
 
 
 to find the monthly data for just January and June 2021.
@@ -63,7 +73,8 @@ as monthly_left_over, total_buyers, price_per_unit, (quantity_supplied*price_per
 as amount_sold, (quantity_produced-quantity_supplied)*price_per_unit as amount_leftover, (quantity_supplied/quantity_produced)*100 
 as percentage_monthly_supplied, (quantity_supplied*price_per_unit)-(quantity_produced-quantity_supplied)*price_per_unit 
 as monthly_revenue, 100-(quantity_supplied/quantity_produced)*100 
-as percentage_monthly_leftover from rice_project.fried_rice_sales_2021;
+as percentage_monthly_leftover 
+from rice_project.fried_rice_sales_2021;
 
 
 
@@ -77,43 +88,52 @@ describe rice_project.fried_rice_sales_2022;
 
 looking at data to be using for 2022
 select ID, month, quantity_produced, quantity_supplied, total_buyers
-select ID, month, quantity_produced, quantity_supplied, total_buyers from rice_project.fried_rice_sales_2022;
+select ID, month, quantity_produced, quantity_supplied, total_buyers 
+from rice_project.fried_rice_sales_2022;
 
 calculate leftover after sales each month for 2022
 select ID, month, quantity_produced, quantity_supplied, total_buyers, (quantity_produced-quantity_supplied) 
-as monthly_left_over from rice_project.fried_rice_sales_2022;
+as monthly_left_over 
+from rice_project.fried_rice_sales_2022;
 
 calculate percentage of quantity supplied each month for 2022
 select ID, month, quantity_produced, quantity_supplied, total_buyers, (quantity_supplied/quantity_produced)*100 
-as percentage_monthly_supplied from rice_project.fried_rice_sales_2022;
+as percentage_monthly_supplied 
+from rice_project.fried_rice_sales_2022;
 
 calculate percentage of quantity leftover each month for 2022
 select ID, month, quantity_produced, quantity_supplied, total_buyers, 100-(quantity_supplied/quantity_produced)*100 
-as percentage_monthly_leftover from rice_project.fried_rice_sales_2022;
+as percentage_monthly_leftover 
+from rice_project.fried_rice_sales_2022;
 
 
 calculate total quantity produced in 2022
 select sum(quantity_produced) 
-as total_yearly_produced from rice_project.fried_rice_sales_2022;
+as total_yearly_produced 
+from rice_project.fried_rice_sales_2022;
 
 
 calculate total quantity supplied in 2022
 select sum(quantity_supplied) 
-as total_yearly_supplied from rice_project.fried_rice_sales_2022;
+as total_yearly_supplied 
+from rice_project.fried_rice_sales_2022;
 
 calculate total quantity of leftover incurred in 2022
 select sum(quantity_produced)-sum(quantity_supplied) 
-as total_yearly_leftover from rice_project.fried_rice_sales_2022;
+as total_yearly_leftover 
+from rice_project.fried_rice_sales_2022;
 
 
 calculate amount sold each month in 2022
 select ID, month, quantity_produced, quantity_supplied, total_buyers, price_per_unit, (quantity_supplied*price_per_unit) 
-as amount_sold from rice_project.fried_rice_sales_2022;
+as amount_sold 
+from rice_project.fried_rice_sales_2022;
 
 
 To calculate monthly revenue in 2022
 select (quantity_supplied*price_per_unit)-(quantity_produced-quantity_supplied)*price_per_unit 
-as monthly_revenue from rice_project.fried_rice_sales_2022;
+as monthly_revenue 
+from rice_project.fried_rice_sales_2022;
 
 
 to find the monthly data for just January and June 2022.
@@ -139,4 +159,5 @@ as amount_sold, (quantity_produced-quantity_supplied)*price_per_unit
 as amount_leftover, (quantity_supplied/quantity_produced)*100 
 as percentage_monthly_supplied, (quantity_supplied*price_per_unit)-(quantity_produced-quantity_supplied)*price_per_unit 
 as monthly_revenue, 100-(quantity_supplied/quantity_produced)*100 
-as percentage_monthly_leftover from rice_project.fried_rice_sales_2022;
+as percentage_monthly_leftover 
+from rice_project.fried_rice_sales_2022;
